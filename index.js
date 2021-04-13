2e2249fcdda1ae30185abe1f08f5ec812df3d56b
Mike <mikewrubel51@gmail.com>
2021-04-13 14:46:52 -0400

    commit tree 6ca21845e399c5fa2109813365382f768d079be4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+function saturdayFun(activity = 'roller-skate'){
+if (activity === undefined){
+activity = 'This Saturday, I want to roller-skate!'
+}
+else{
+activity = 'This Saturday, I want to ${activity}!'
+}
+return activity = 'This Saturday, I want to roller-skate!'
+}
+
+function mondayWork(toDo = 'go to the office'){
+if (toDo === undefined){
+    toDo = 'This Monday, I will go to the office'
+}
+else{
+    toDo = 'This Monday I will ${toDo}'
+}
+return 'This Monday, I will ${toDo}' 
+}
+
+function wrapAdjective(asteric='*'){
+    const encouragingPrompt = 'special'
+return 'You are ${asteric}${encouragingPrompt}${asteric}!'
+   // return encouragingPrompt
+}
